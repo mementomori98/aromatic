772b725cfe3958afe352aea11470cc7496a0f77a
mementox <sebestyen.mate98@gmail.com>
2020-04-25 17:51:19 +0200

    Configure heroku

--- src/main.dart
+++ src/main.dart
@@ -4,6 +4,20 @@ typedef void Handler(HttpRequest request);
 
 main() async {
   var server = await HttpServer.bind(InternetAddress.loopbackIPv4, 4040);
+
+  var controller = getController();
+  await for (HttpRequest request in server) {
+    print("${request.method} ${request.uri}");
+    request.response.headers.contentType =
+        new ContentType("text", "html", charset: "utf-8");
+    if (controller.containsKey(request.uri.toString()))
+      controller[request.uri.toString()](request);
+    else
+      controller["/error"](request);
+  }
+}
+
+Map<String, Function> getController() {
   var map = {
     ["/", "/home", "/index"]: home,
     ["/error"]: error
@@ -11,22 +25,16 @@ main() async {
   var handlers = {
     for (var entry in map.entries) for (var path in entry.key) path: entry.value
   };
-  print(handlers);
-  await for (HttpRequest request in server) {
-    print("${request.method} ${request.uri}");
-    if (handlers.containsKey(request.uri.toString()))
-      handlers[request.uri.toString()](request);
-    else
-      handlers["/error"](request);
-  }
+  return handlers;
 }
 
 void home(HttpRequest request) async {
-  request.response.write("<h1>Welcome to AroMatic</h1>");
+  request.response.write('<h1>Welcome to AroMatic</h1>');
   await request.response.close();
 }
 
 void error(HttpRequest request) async {
-  request.response.write("<h1>An error occured</h1>");
+  request.response.write('<h1">An error occured</h1>');
+
   await request.response.close();
 }
